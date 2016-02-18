6cdb2bf974ff8d02f4233f342fe6bdc898194a67
Vu Ha <stregawolf@gmail.com>
2016-02-18 01:11:22 -0800

    added banking system

---
+++ VDayJam2016/Assets/VDayJam2016/Scripts/Gameplay/Environment/BankAmountDisplay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BankAmountDisplay : MonoBehaviour {
+    public TextMesh mText;
+
+	protected void Awake()
+    {
+        OnBankAmountChanged();
+        Signal.Register(SignalType.BankedHeartAmountChanged, OnBankAmountChanged);
+    }
+
+    protected void OnDestroy()
+    {
+        Signal.Unregister(SignalType.BankedHeartAmountChanged, OnBankAmountChanged);
+    }
+
+    public void OnBankAmountChanged()
+    {
+        mText.text = string.Format("Banked Hearts:\n{0}", GlobalData.NumBankedHearts);
+    }
+}
