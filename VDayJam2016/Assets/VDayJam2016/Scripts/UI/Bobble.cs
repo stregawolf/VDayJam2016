4eac13846d0ff2918723c7883646cf92033f97a4
Vu Ha <stregawolf@gmail.com>
2016-03-03 00:29:01 -0800

    hooked up all new art assets
    hooked up win screens

---
+++ VDayJam2016/Assets/VDayJam2016/Scripts/UI/Bobble.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bobble : MonoBehaviour {
+    public float mBobbleSpeed = 1.0f;
+    public float mBobbleHeight = 50.0f;
+    public float mT = 0.0f;
+    protected Vector3 mStartPos;
+
+    void Start()
+    {
+        mStartPos = transform.localPosition;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        mT += Time.deltaTime * mBobbleSpeed;
+        transform.localPosition = mStartPos + Vector3.up * mBobbleHeight * Mathf.Cos(mT);
+	}
+}
--- VDayJam2016/Assets/VDayJam2016/Scripts/UI/WinScreen.cs
+++ VDayJam2016/Assets/VDayJam2016/Scripts/UI/WinScreen.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
